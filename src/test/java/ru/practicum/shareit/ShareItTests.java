b8fafbbce0de8f8a62bd7afc51bedf86648de257
Aleksey <aleksiei.ivin.97@yandex.ru>
2024-08-15 23:08:23 +0300

    test context

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,10 +2,8 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(classes = ShareItTests.class)
 class ShareItTests {
 
 	@Test
